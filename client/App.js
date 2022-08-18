30b8fc76b379d0d93a1dd73bd2de9dbc2bdfe06d
avachoi <alexbchoi@gmail.com>
2022-08-17 23:33:31 -0400

    succeed http request

--- client/App.js
+++ client/App.js
@@ -1,13 +1,42 @@
 import React from "react";
+import axios from "axios";
 
-import Routes from "./Routes";
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			members: [],
+		};
+	}
+	async componentDidMount() {
+		try {
+			const response = await axios.get("api/users");
+			const members = response.data;
+			this.setState({ members: members });
+		} catch (error) {
+			throw new Error("Error");
+		}
+	}
+	render() {
+		const { members } = this.state;
+		return (
+			<div>
+				<h1>members</h1>
 
-const App = () => {
-	return (
-		<div>
-			<Routes />
-		</div>
-	);
-};
+				{members[0] ? (
+					<div>
+						<ul>
+							{members.map((member) => (
+								<li>{member.firstName}</li>
+							))}
+						</ul>
+					</div>
+				) : (
+					<div>loading</div>
+				)}
+			</div>
+		);
+	}
+}
 
 export default App;
