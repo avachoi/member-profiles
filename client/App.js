3752cd5f271aee21ba39d330f00133e0589afebf
avachoi <alexbchoi@gmail.com>
2022-08-19 23:36:22 -0400

    added inputBox

--- client/App.js
+++ client/App.js
@@ -1,42 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import MemberList from "./MemberList";
 
-class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			members: [],
-		};
-	}
-	async componentDidMount() {
-		try {
-			const response = await axios.get("api/users");
-			const members = response.data;
-			this.setState({ members: members });
-		} catch (error) {
-			throw new Error("Error");
-		}
-	}
-	render() {
-		const { members } = this.state;
-		return (
-			<div>
-				<h1>members</h1>
+const App = (props) => {
+	let [members, setMembers] = useState([]);
 
-				{members[0] ? (
-					<div>
-						<ul>
-							{members.map((member) => (
-								<li>{member.firstName}</li>
-							))}
-						</ul>
-					</div>
-				) : (
-					<div>loading</div>
-				)}
+	useEffect(() => {
+		const response = axios.get("api/users");
+		const people = response.data;
+		setMembers(people);
+	});
+
+	// async componentDidMount() {
+	// 	try {
+	// 		const response = await axios.get("api/users");
+	// 		const members = response.data;
+	// 		this.setState({ members: members });
+	// 	} catch (error) {
+	// 		throw new Error("Error");
+	// 	}
+	// }
+
+	return (
+		<div>
+			<h1>members!!</h1>
+			<div className="inputBoxes">
+				<form>
+					<input
+						placeholder="Search by name"
+						name="searchName"
+						// value={searchName}
+						// onChange={handleChange}
+					/>
+					<input
+						placeholder="Search by tag"
+						name="searchTag"
+						// value={searchName}
+						// onChange={handleChange}
+					/>
+				</form>
 			</div>
-		);
-	}
-}
+
+			<MemberList />
+
+			{members[0] ? (
+				<div>
+					<ul>
+						{members.map((member, i) => (
+							<li key={i}>{member.firstName}</li>
+						))}
+					</ul>
+				</div>
+			) : (
+				<div>loading</div>
+			)}
+		</div>
+	);
+};
 
 export default App;
